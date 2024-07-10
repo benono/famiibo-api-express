bad6650a63c36548547148efe7b28ad60d16c099
BenOno <bentoki.1213@gmail.com>
2024-07-09 23:05:24 -0700

    Feat add register api

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
 const express = require('express');
+const { PrismaClient } = require('@prisma/client');
+const bcrypt = require('bcrypt');
+
 const app = express();
 const PORT = 8000
+const prisma = new PrismaClient();
 
-app.get('/', (req, res) => {
+// Register user
+app.post('/api/auth/register', async (req, res) => {
+  console.log('start')
+  console.log(req.body)
+  const {username, email, password} = req.body;
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+  const user = await prisma.user.create({
+    data: {
+      username,
+      email,
+      password: hashedPassword
+    }
+  })
   res.send('<h1>Hello World</h1>');
 });
 
