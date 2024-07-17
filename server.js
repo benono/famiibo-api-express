fc96adba6388af11d7274a77c58da3875e6573b9
BenOno <bentoki.1213@gmail.com>
2024-07-17 01:04:27 -0700

    Add account post method

--- server.js
+++ server.js
@@ -2,14 +2,19 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const authRouter = require('./app/routers/auth');
+const accountRouter = require('./app/routers/account');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:3001',
+  credentials: true
+}));
 app.use(express.json());
 const PORT = 8888
 
 app.use('/api/auth', authRouter);
+app.use('/api/accounts', accountRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
